ba3f0dea503e8a257ddd50487391e68dd877aafa
Ben Smith <bh2smith@gmail.com>
2023-01-27 19:28:15 +0100

    fix lambda so it works

--- tenderly/actions/src/lambda.ts
+++ tenderly/actions/src/lambda.ts
@@ -1,20 +1,43 @@
-import axios from 'axios';
-export async function invokeLambda(
+import axios from "axios";
+async function invokeLambda(
   functionUrl: string,
   txHash: string,
   solver: string
 ) {
   console.log(`Invoking AWS Lambda Pipeline for (${txHash}, ${solver})`);
-  const response = await axios.post("https://reqres.in/api/users", {
-    method: "POST",
-    body: JSON.stringify({
-      txHash: txHash,
-      solver: solver,
-    }),
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
-  console.log(response.status)
+  try {
+    const response = await axios.post(
+      functionUrl,
+      {
+        txHash: txHash,
+        solver: solver,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    console.log(`Lambda response: ${response.data}`);
+  } catch (error: any) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else if (error.message) {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    } else {
+      console.log(error);
+    }
+  }
 }
+
+export { invokeLambda };
